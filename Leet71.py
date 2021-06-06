6590c3f162eef10fef393b74f97b62e300afc19e
DGnzl <dnlgonzal@outlook.com>
2021-06-05 20:59:38 -0700

    Added unti testing to 71

--- Leet71.py
+++ Leet71.py
@@ -1,3 +1,4 @@
+import unittest
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
@@ -15,3 +16,16 @@ print(Solution.simplifyPath(Solution, "/a/./b/../../c/"))
 print(Solution.simplifyPath(Solution, "/home//foo/"))
 print(Solution.simplifyPath(Solution, "/../"))
 print(Solution.simplifyPath(Solution, "/home/"))
+
+class Testing(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(Solution.simplifyPath(Solution, "/a/./b/../../c/"), "/c")
+    def test2(self):
+        self.assertEqual(Solution.simplifyPath(Solution, "/home//foo/"), "/home/foo")
+    def test3(self):
+        self.assertEqual(Solution.simplifyPath(Solution, "/../"), "/")
+    def test4(self):
+        self.assertEqual(Solution.simplifyPath(Solution, "/home/"), "/home")
+
+if __name__ == "__main__":
+    unittest.main()
