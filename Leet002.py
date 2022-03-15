064bb6651c67b0896eb4f3decdaf8b3c31d936d6
DGnzl <dnlgonzal@outlook.com>
2022-03-14 21:32:24 -0700

    Leet002

--- Leet002.py
+++ Leet002.py
@@ -6,10 +6,8 @@ def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optio
             num1 = l1.val if l1 is not None else 0
             num2 = l2.val if l2 is not None else 0
             itr.val = num1 + num2 + carryOver
-            carryOver = 0
-            if itr.val > 9:
-                carryOver = 1
-                itr.val -= 10
+            carryOver = itr.val // 10
+            itr.val = itr.val % 10
             l1 = l1.next if l1 is not None else l1
             l2 = l2.next if l2 is not None else l2
             if l1 or l2:
